c88af7b13320ff9f1843c8178c77e0cff40ae70f
Molnár Dávid <d.molnarka@hotmail.com>
2023-04-24 17:23:24 +0200

    Implement 'Resize grid' button

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 const container = document.querySelector("#container");
+const gridWidthAndHeight = 800;
+let gridItems;
 
-function createGrid() {
-    for (let i=0; i<16; i++) {
+function changeBackgroundColor() {
+    this.style.backgroundColor = "black";
+}
+
+function clearGrid() {
+    container.textContent = "";
+}
+
+function createGrid(gridSize = 16) {
+    clearGrid();
+
+    for (let i=0; i<gridSize; i++) {
         const divRow = document.createElement("div");
-        for (let j=0; j<16; j++) {
+        for (let j=0; j<gridSize; j++) {
             const divColumn = document.createElement("div");
             divColumn.classList.add("grid-item");
-            divColumn.style.width = "32px";
-            divColumn.style.height = "32px";
+            const gridItemWidthAndHeight = gridWidthAndHeight/gridSize;
+            divColumn.style.width = `${gridItemWidthAndHeight}px`;
+            divColumn.style.height = `${gridItemWidthAndHeight}px`;
             divRow.appendChild(divColumn);
         }
         divRow.style.display = "flex";
         container.appendChild(divRow);
     }
-}
+    gridItems = document.querySelectorAll(".grid-item");
+
+    gridItems.forEach((gridItem) => {
+        gridItem.addEventListener("mouseover", changeBackgroundColor)
+    });
+};
 
 createGrid();
-const gridItems = document.querySelectorAll(".grid-item");
-gridItems.forEach((gridItem) => {
-    gridItem.addEventListener("mouseover", () => {
-        gridItem.style.backgroundColor = "black";
-    })
+
+const btnResizeGrid = document.querySelector("#btn-resize-grid");
+btnResizeGrid.addEventListener("click", (e) => {
+    createGrid(Math.min(prompt("Enter new grid size:", 16), 100));
 });
